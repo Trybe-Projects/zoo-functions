79d027fa8aa971285a562679794867ea1331c59d
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-05-13 19:14:59 -0300

    completed requirement 4

--- src/zoo.js
+++ src/zoo.js
@@ -31,6 +31,7 @@ function employeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return {...personalInfo, ...associatedWith};
 }
 
 function isManager(id) {
